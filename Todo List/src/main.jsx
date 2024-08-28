930e18e7cda90c4f2a9dfc59a866fdcc876b8e98
Neharika357 <153827354+Neharika357@users.noreply.github.com>
2024-08-28 23:05:44 +0530

    Create main.jsx

---
+++ Todo List/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import To_Do_List1 from './ToDoList1'
+import To_Do_List2 from './TODoList2'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <To_Do_List2/>
+  </React.StrictMode>,
+)
